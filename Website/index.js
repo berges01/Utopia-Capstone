1982daa767d9e0e8ddb4077b1e08be05faf3f80c
Erik Berges <erik.t.berges@gmail.com>
2024-02-07 21:58:18 -0700

    changes

--- Website/index.js
+++ Website/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 
 // The service port may be set on the command line
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
